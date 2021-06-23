606e42a8b801add8daf0f55632439cb04ab93a72
Siddharth194 <83445812+Siddharth194@users.noreply.github.com>
2021-06-23 11:42:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -32,22 +32,35 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    pygame.key.get_pressed()
+
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_RIGHT:
-            playerX += 32
-            pimg = playerimgR
+            if (playerY > -33 and playerY < 97 and playerX == 256) or (playerY > 383 and playerY < 513 and playerX == 288) or playerX == 768:
+                playerX += 0
+            else:
+                playerX += 32
+                pimg = playerimgR
 
         elif event.key == pygame.K_LEFT:
-            playerX -= 32
-            pimg = playerimgL
+            if (playerY > -33 and playerY < 65 and playerX == 96) or (playerY > 95 and playerY < 225 and playerX ==160) or (playerY > 383 and playerY < 513 and playerX == 640) or (playerY > 383 and playerY < 545 and playerX == 160) or playerX == -32:
+                playerX += 0
+            else:
+                playerX -= 32
+                pimg = playerimgL
 
         elif event.key == pygame.K_UP:
-            playerY -= 32
-            pimg = playerimgB
+            if (playerX > -1 and playerX < 129 and playerY == 256) or (playerX > 287 and playerX < 769 and playerY == 128) or playerY == -32:                playerY += 0
+            else:
+                playerY -= 32
+                pimg = playerimgB
 
         elif event.key == pygame.K_DOWN:
-            playerY += 32
-            pimg = playerimg
+            if (playerX > 319 and playerX < 609 and playerY == 352) or (playerX > -33 and playerX < 129 and playerY == 352) or playerY == 544:
+                playerY += 0
+            else:
+                playerY += 32
+                pimg = playerimg
 
 
     if event.type == pygame.KEYUP:
@@ -55,6 +68,9 @@ while running:
         playerX+=0
 
     screen.fill((0,0,0))
+
+    print("X = ", playerX)
+    print("Y = ", playerY)
 
     mapdraw()
 
