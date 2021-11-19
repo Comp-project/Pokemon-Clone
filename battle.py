4567d790166cd0dac8d25aceb0c22cbc10f31206
Siddharth194 <83445812+Siddharth194@users.noreply.github.com>
2021-11-19 06:30:25 +0530

    Update battle.py

--- battle.py
+++ battle.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+
 pygame.init()
 
 bbimg = pygame.image.load('Images/bulback.png')
@@ -14,39 +16,94 @@ greenbars = pygame.image.load('Images/greenbar.png')
 arrow = pygame.image.load('Images/arrowcursor.png')
 #all the images added above
 
-greenbar2 = greenbars.copy()
+greenbar2 = pygame.image.load('Images/greenbar.png')
 #duplicating health bar - one for player's health, the other for opponent
 
 fightstate = 1
 #a fight state variable - will be 1 when the fight is going on, will be 2 if player wins, will be 3 if opponent wins ( will tell more about this tomorrow)
-
+greenbarlength = 168
 running = True
+
+playerhealth = 100
+pchealth = 100
+damage = -20 - (random.random()*10)//1
+heal = +25
 
 screen = pygame.display.set_mode((800,600))
 
+def pattack():
+    global pchealth
+    pchealth += -20 - (random.random()*10)//1
+    if pchealth <= 0:
+        pchealth = 0
+
+def pheal():
+    global playerhealth
+    playerhealth += 20 + (random.random()*10)//1
+    if playerhealth >= 100:
+        playerhealth = 100
+
+def pcattack():
+    global playerhealth
+    playerhealth += -20 - (random.random()*10)//1
+    if playerhealth <= 0:
+        playerhealth = 0
+
+def pcheal():
+    global pchealth
+    pchealth += 20 + (random.random()*10)//1
+    if pchealth >= 100:
+        pchealth = 100
 arrowcoordinatex, arrowcoordinatey = 40,520
 #coordinates of the arrow cursor to navigate between options)
 
 while running:
+
+    bannertextvar = True
+
+    temppch, tempplh, playerdamagedealt, pcdamagedealt, playerheal, pchealed = 0,0,0,0,0,0
+
     screen.blit(bsc, (0,0))
 
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            running = False
-
     screen.blit(pikaimg, (450, 60))
-    screen.blit(cbimg, (75,275))
-    screen.blit(pbsc, (0,450))
+    screen.blit(cbimg, (75, 275))
+    screen.blit(pbsc, (0, 450))
     font = pygame.font.Font('Raleway-Medium.ttf', 30)
     font2 = pygame.font.Font('PokemonGb-RAeo.ttf', 20)
-    text4 = font2.render('Pikachu',True, (0, 0, 0))
+    text4 = font2.render('Pikachu', True, (0, 0, 0))
     text5 = font2.render('Charmander', True, (0, 0, 0))
-    screen.blit(hpbars, (0,0))
+    screen.blit(hpbars, (0, 0))
     screen.blit(greenbars, (165, 115))
     screen.blit(greenbar2, (601, 411))
-    screen.blit(text4,(34,69))
+    screen.blit(text4, (34, 77))
     screen.blit(text5, (466, 372))
+
+    ph1 = playerhealth
+    pch1 = pchealth
+    if pchealth == 0:
+        while True:
+            screen.blit(bsc, (0,0))
+            screen.blit(cbimg, (75,275))
+            screen.blit(pbsc, (0,450))
+            screen.blit(hpbars, (0, 0))
+            screen.blit(greenbars, (165, 115))
+            screen.blit(greenbar2, (601, 411))
+            text8 = font2.render('The wild Pikachu fainted!', True, (0,0,0))
+            screen.blit(text8, (40,505))
+            pygame.display.update()
+    elif playerhealth == 0:
+        while True:
+            screen.blit(bsc, (0, 0))
+            screen.blit(pbimg, (75, 275))
+            screen.blit(pbsc, (0, 450))
+            screen.blit(hpbars, (0, 0))
+            screen.blit(greenbars, (165, 115))
+            screen.blit(greenbar2, (601, 411))
+            text8 = font2.render('Charmander fainted!', True, (0,0,0))
+            screen.blit(text8, (40, 505))
+            pygame.display.update()
+
+    #2 variables for the initial values of health, used later for health bar
 
     if fightstate == 1:
         text1 = font.render('FIGHT', True, (0,0,0))
@@ -57,17 +114,109 @@ while running:
         screen.blit(text3, (675, 510))
         screen.blit(arrow, (arrowcoordinatex,arrowcoordinatey))
 
-        for event1 in pygame.event.get():
-            if event1.type == pygame.KEYDOWN:
-                if event1.key == pygame.K_RIGHT:
+    for event1 in pygame.event.get():
+
+        if event1.type == pygame.QUIT:
+            running = False
+
+        if event1.type == pygame.KEYDOWN:
+            if event1.key == pygame.K_RIGHT:
+
                     if arrowcoordinatex != 640:
                         screen.blit(arrow, (arrowcoordinatex + 300,arrowcoordinatey))
                         arrowcoordinatex += 300
                     else:
                         screen.blit(arrow, (40,arrowcoordinatey))
                         arrowcoordinatex = 40
-                        
-        #above code is for controlling the cursor arrow
 
+            if event1.key == pygame.K_a:
+
+                if arrowcoordinatex == 40:
+                    pattack()
+
+                    playerdamagedealt = str(pch1-pchealth)
+                    temppch = pchealth
+
+                    pcmovedetermine = random.random()
+
+                    if pcmovedetermine > 0.5:
+
+                        pcattack()
+
+                        pcdamagedealt = str(ph1 - playerhealth)
+
+                    else:
+                        pcheal()
+
+                        pchealed = str(pchealth-temppch)
+
+
+
+                if arrowcoordinatex == 340:
+                    pheal()
+
+                    tempplh = playerhealth
+
+                    playerheal = str(playerhealth - ph1)
+
+                    pcmovedetermine = random.random()
+
+                    if pcmovedetermine > 0.5:
+                        pcattack()
+
+                        pcdamagedealt = str(tempplh - playerhealth)
+
+
+                    else:
+                        pcheal()
+
+                        pchealed = str(pchealth - pch1)
+
+            print('player', playerhealth)
+            print('opponent', pchealth)
+
+
+    if temppch != 0 or tempplh != 0:
+        screen.blit(pbsc, (0,450))
+
+        if tempplh == 0:
+            text6 = 'You dealt {} HP worth of damage.'.format(playerdamagedealt)
+
+            if pcmovedetermine>0.5:
+                text7 = 'The opponent dealt {} HP worth of damage to you.'.format(pcdamagedealt)
+            else:
+                text7 = 'The opponent healed {} HP.'.format(pchealed)
+
+        else:
+            text6 = 'You healed {} HP'.format(playerheal)
+
+            if pcmovedetermine>0.5:
+                text7 = 'The opponent dealt {} HP worth of damage to you.'.format(pcdamagedealt)
+            else:
+                text7 = 'The opponent healed {} HP.'.format(pchealed)
+
+        blittext6 = font2.render(text6, True, (0,0,0))
+        blittext7 = font2.render(text7, True, (0,0,0))
+
+        while True:
+            screen.blit(blittext6, (40,478))
+            screen.blit(blittext7, (40,544))
+            pygame.display.update()
+            for event2 in pygame.event.get():
+                if event2.type == pygame.KEYDOWN:
+                    bannertextvar = False
+                    break
+
+            if not bannertextvar:
+                break
+
+    playerhpprint = font2.render('{}/100'.format(str(playerhealth)), True, (0,0,0))
+    pchpprint = font2.render('{}/100'.format(str(pchealth)), True, (0,0,0))
+    screen.blit(playerhpprint, (610,440))
+    screen.blit(pchpprint, (220,77))
+    greenbars = pygame.transform.scale(greenbars, ((pchealth / 100) * 168, 16))
+    greenbar2 = pygame.transform.scale(greenbar2, ((playerhealth/100)*168, 16))
+
+        #above code is for controlling the cursor arrow
 
     pygame.display.update()
